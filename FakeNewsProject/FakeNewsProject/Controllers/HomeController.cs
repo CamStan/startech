4668767d94cdbbc123178550a03f6c7f019ea78b
Wayne <none>
2017-01-27 19:00:50 -0800

    Created story creation page

--- FakeNewsProject/FakeNewsProject/Controllers/HomeController.cs
+++ FakeNewsProject/FakeNewsProject/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Diagnostics;
 
 namespace FakeNewsProject.Controllers
 {
@@ -15,6 +16,39 @@ namespace FakeNewsProject.Controllers
         public ActionResult Index()
         {
             return View(db.Stories.OrderByDescending(s => s.PostDate).ToList());
+        }
+
+        /// <summary>
+        /// HttpGet method
+        /// Shows story creation page. Will need to set a way to get user's ID
+        /// from log in. Story model requires the user's ID.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Story postSetup = new Story();
+            postSetup.UserID = 1;
+            postSetup.PostDate = DateTime.Now;
+            return View(postSetup);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Story newPost)
+        {
+            Debug.WriteLine("We're at the post!");
+            Debug.WriteLine(newPost.ID);
+            Debug.WriteLine(newPost.PostDate);
+
+            if (ModelState.IsValid)
+            {
+                Debug.WriteLine("We are saving!");
+                db.Stories.Add(newPost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            Debug.WriteLine("We missed the save.");
+            return View(newPost);
         }
     }
 }
