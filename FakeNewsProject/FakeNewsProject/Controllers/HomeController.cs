ade0a0c2d187d5a1530ff79c14ca1c37d19c88c9
CStanavige <cstanavige12@wou.edu>
2017-01-27 17:29:37 -0800

    Created dbcontext, models, homecontroller, and home/index view. Includes upload of other relevant files

---
+++ FakeNewsProject/FakeNewsProject/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using FakeNewsProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FakeNewsProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        FakeNewsContext db = new FakeNewsContext();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Stories.OrderByDescending(s => s.PostDate).ToList());
+        }
+    }
+}
---
+++ FakeNewsProject/FakeNewsProject/Models/User.cs
@@ -0,0 +1,36 @@
+namespace FakeNewsProject.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            Stories = new HashSet<Story>();
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Author")]
+        public string UserName { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Story> Stories { get; set; }
+    }
+}
