93d0aac4edaa1864d2fe66b8e93ca66c4426a6c2
CStanavige <cstanavige12@wou.edu>
2017-03-05 21:47:10 -0800

    Set up repository to be used for dependency injection with the admin portal

---
+++ IPGMMS/IPGMMS/DAL/Repositories/EFPortalRepository.cs
@@ -0,0 +1,23 @@
+﻿using IPGMMS.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPGMMS.DAL.Repositories
+{
+    public class EFPortalRepository : IPortalRepository
+    {
+        private IPGMMS_Context db;
+
+        /// <summary>
+        /// Constructor for this repository. Sets the dbcontext for this class with the injected context
+        /// </summary>
+        /// <param name="context">The dbContext to use for working with members</param>
+        public EFPortalRepository(IPGMMS_Context context)
+        {
+            db = context;
+        }
+
+    }
+}
