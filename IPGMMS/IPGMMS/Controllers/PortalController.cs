cb9263c0c02c65e1a67bff23f4047746c3e93e6b
CStanavige <cstanavige12@wou.edu>
2017-03-05 23:27:30 -0800

    Refactored PortalController to use IoC with a PortalRepo and to only allow admin access. Need to look into whether to add UserManager field and method to the controller or if its better to do that through the repository.

--- IPGMMS/IPGMMS/Controllers/PortalController.cs
+++ IPGMMS/IPGMMS/Controllers/PortalController.cs
@@ -1,4 +1,5 @@
-﻿using IPGMMS.Models;
+﻿using IPGMMS.Abstract;
+using IPGMMS.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
@@ -9,49 +10,56 @@ using System.Web.Mvc;
 
 namespace IPGMMS.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Admin")]
     public class PortalController : Controller
     {
-        // Check if admin
-        public Boolean isAdminUser()
+        private IPortalRepository repo;
+
+        public PortalController(IPortalRepository portalRepo)
         {
-            if (User.Identity.IsAuthenticated)
-            {
-                var user = User.Identity;
-                ApplicationDbContext context = new ApplicationDbContext();
-                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-                var s = UserManager.GetRoles(user.GetUserId());
-                if (s[0].ToString() == "Admin")
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return false;
+            repo = portalRepo;
         }
+
+        // Check if admin
+        //public Boolean isAdminUser()
+        //{
+        //    if (User.Identity.IsAuthenticated)
+        //    {
+        //        var user = User.Identity;
+        //        ApplicationDbContext context = new ApplicationDbContext();
+        //        var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+        //        var s = UserManager.GetRoles(user.GetUserId());
+        //        if (s[0].ToString() == "Admin")
+        //        {
+        //            return true;
+        //        }
+        //        else
+        //        {
+        //            return false;
+        //        }
+        //    }
+        //    return false;
+        //}
 
         // GET: Portal
         public ActionResult Index()
         {
-            if(User.Identity.IsAuthenticated)
-            {
-                var user = User.Identity;
-                ViewBag.Name = user.Name;
-                ViewBag.displayAdmin = "No";
+            //if(User.Identity.IsAuthenticated)
+            //{
+            var user = User.Identity;
+            ViewBag.Name = user.Name;
+            //    ViewBag.displayAdmin = "No";
 
-                if(isAdminUser())
-                {
-                    ViewBag.displayAdmin = "Yes";
-                }
-                return View();
-            }
-            else
-            {
-                ViewBag.Name = "Not Logged In";
-            }
+            //    if(isAdminUser())
+            //    {
+            //        ViewBag.displayAdmin = "Yes";
+            //    }
+            //    return View();
+            //}
+            //else
+            //{
+            //    ViewBag.Name = "Not Logged In";
+            //}
             return View();
         }
     }
--- IPGMMS/IPGMMS/DAL/Repositories/EFPortalRepository.cs
+++ IPGMMS/IPGMMS/DAL/Repositories/EFPortalRepository.cs
@@ -1,4 +1,5 @@
 ﻿using IPGMMS.Abstract;
+using IPGMMS.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,17 @@ namespace IPGMMS.DAL.Repositories
     public class EFPortalRepository : IPortalRepository
     {
         private IPGMMS_Context db;
+        private ApplicationDbContext adb;
 
         /// <summary>
         /// Constructor for this repository. Sets the dbcontext for this class with the injected context
         /// </summary>
-        /// <param name="context">The dbContext to use for working with members</param>
-        public EFPortalRepository(IPGMMS_Context context)
+        /// <param name="context">The IPG dbContext to use for working with the admin portal</param>
+        /// <param name="appContext">The application dbContext to use for working with the admin portal</param>
+        public EFPortalRepository(IPGMMS_Context context, ApplicationDbContext appContext)
         {
             db = context;
+            adb = appContext;
         }
 
     }
