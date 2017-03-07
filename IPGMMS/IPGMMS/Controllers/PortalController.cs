a1e59ef5dc2254ffdeb34fa5834ff3e2e714127c
CStanavige <cstanavige12@wou.edu>
2017-03-07 15:32:58 -0800

    Switched admin portal access back to what it was

--- IPGMMS/IPGMMS/Controllers/PortalController.cs
+++ IPGMMS/IPGMMS/Controllers/PortalController.cs
@@ -10,7 +10,7 @@ using System.Web.Mvc;
 
 namespace IPGMMS.Controllers
 {
-    [Authorize(Roles = "Admin")]
+    //[Authorize(Roles = "Admin")]
     public class PortalController : Controller
     {
         private IPortalRepository repo;
@@ -20,46 +20,46 @@ namespace IPGMMS.Controllers
             repo = portalRepo;
         }
 
-        // Check if admin
-        //public Boolean isAdminUser()
-        //{
-        //    if (User.Identity.IsAuthenticated)
-        //    {
-        //        var user = User.Identity;
-        //        ApplicationDbContext context = new ApplicationDbContext();
-        //        var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-        //        var s = UserManager.GetRoles(user.GetUserId());
-        //        if (s[0].ToString() == "Admin")
-        //        {
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //    return false;
-        //}
+        //Check if admin
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
 
         // GET: Portal
         public ActionResult Index()
         {
-            //if(User.Identity.IsAuthenticated)
-            //{
-            var user = User.Identity;
-            ViewBag.Name = user.Name;
-            //    ViewBag.displayAdmin = "No";
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+                ViewBag.displayAdmin = "No";
 
-            //    if(isAdminUser())
-            //    {
-            //        ViewBag.displayAdmin = "Yes";
-            //    }
-            //    return View();
-            //}
-            //else
-            //{
-            //    ViewBag.Name = "Not Logged In";
-            //}
+                if (isAdminUser())
+                {
+                    ViewBag.displayAdmin = "Yes";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged In";
+            }
             return View();
         }
     }
