0ac1fce76ae3acd12818ae906d40647cb6f8f320
Wayne <none>
2017-04-05 14:45:47 -0700

    initial commit

---
+++ IPGMMS/IPGMMS/Models/MemberCreate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IPGMMS.Models
+{
+    public class MemberCreate
+    {
+        public Member MemberInfo { get; set; }
+        public ContactInfo MailingInfo { get; set; }
+        public ContactInfo ListingInfo { get; set; }
+        public MemberLevel Levels { get; set; }
+
+    }
+}
