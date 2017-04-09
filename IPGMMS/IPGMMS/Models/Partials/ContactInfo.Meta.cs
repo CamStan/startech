4571549918c8ee62bad77dc1efe7dd4bc23c4274
CStanavige <cstanavige12@wou.edu>
2017-04-09 12:00:07 -0700

    Added/fixed meta data for the membercreate form and made minor changes to the form

---
+++ IPGMMS/IPGMMS/Models/Partials/ContactInfo.Meta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IPGMMS.Models
+{
+    [MetadataType(typeof(ContactInfoMetadata))]
+    public partial class ContactInfo
+    {
+    }
+
+    public class ContactInfoMetadata
+    {
+        [Display(Name = "Street Address")]
+        public string StreetAddress;
+
+        [Display(Name = "State")]
+        public string StateName;
+
+        [Display(Name = "Country Code")]
+        public string Country;
+
+        [Display(Name = "Postal Code")]
+        public string PostalCode;
+
+        [Display(Name = "Phone #")]
+        public string PhoneNumber;
+    }
+}
